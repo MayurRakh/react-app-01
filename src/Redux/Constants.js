b5c1635f3ff9d3e3a074fa8d301ef32e7ac7854d
Mukesh Rakh <mukesh10.rakh@gmail.com>
2023-12-12 17:32:07 +0530

    port change

--- src/Redux/Constants.js
+++ src/Redux/Constants.js
@@ -3,7 +3,7 @@
 // export const URL = "http://52.66.247.7:6001";
 // export const URL = "http://localhost:6001";
 
-export const URL = "http://3.109.123.153:31907"
+export const URL = "http://3.109.123.153:30756"
 //+++++++++ Constants +++++++++++++
 
 export const USER_CLEAR_STATE = "USER_CLEAR_STATE";
